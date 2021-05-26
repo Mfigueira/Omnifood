eaf6943a1a1ca66bde908d19de5ed404be90810a
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-05-26 15:44:59 -0300

    js update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// *** Selectors
+// *** Elements/Selectors
 // * Buttons/Links
-const mainBtn = document.querySelector('.hero-text-box .btn-full');
-const secondaryBtn = document.querySelector('.hero-text-box .btn-ghost');
+const btnMain = document.querySelector('.hero-text-box .btn-full');
+const btnSecondary = document.querySelector('.hero-text-box .btn-ghost');
+const btnBackToTop = document.querySelector('.logo-sticky');
+const btnMobileNav = document.querySelector('.mobile-nav-btn');
 const linkToFood = document.querySelector('.link-to-food');
 const linkToHowWorks = document.querySelector('.link-to-how-works');
 const linkToCities = document.querySelector('.link-to-cities');
 const linkToSignUp = document.querySelector('.link-to-sign-up');
-const backToTopBtn = document.querySelector('.logo-sticky');
-const mobileNavBtn = document.querySelector('.mobile-nav-btn');
 // * Sections
 const header = document.querySelector('header');
 const sectionFeatures = document.querySelector('.section-features');
@@ -28,49 +28,43 @@ const navUl = document.querySelector('nav ul');
 const navigation = document.querySelector('nav');
 const stickyPoint = sectionFeatures.offsetTop;
 
-// *** Scroll Event
-window.onscroll = () => {
-  // Sticky navigation logic
-  if (window.pageYOffset >= stickyPoint) {
-    navigation.classList.add('sticky-transition');
-    setTimeout(() => {
-      navigation.classList.add('sticky');
-      navigation.classList.remove('sticky-transition');
-    }, 150);
-
-    // Animate Features
-    animatedFeatures.forEach((el, i) => {
-      const anim = el =>
-        el.classList.add('animate__animated', 'animate__fadeInUp');
-      setTimeout(() => anim(el), i * 150);
-    });
-  } else {
-    navigation.classList.remove('sticky');
-  }
-
-  // Animate How it Works
-  if (window.pageYOffset >= sectionHowWorks.offsetTop - 50)
-    animatedMobile.classList.add('animate__animated', 'animate__fadeInLeft');
-
-  // Animate Cities
-  if (window.pageYOffset >= sectionCities.offsetTop - 50) {
-    animatedCities.forEach((el, i) => {
-      const anim = el =>
-        el.classList.add('animate__animated', 'animate__fadeIn');
-      setTimeout(() => anim(el), i * 200);
-    });
-  }
-
-  // Animate Plan
-  if (window.pageYOffset >= sectionPlans.offsetTop - 50)
-    animatedPlan.classList.add('animate__animated', 'animate__pulse');
+// *** Animations
+const stickHeader = () => {
+  navigation.classList.add('sticky-transition');
+  setTimeout(() => {
+    navigation.classList.add('sticky');
+    navigation.classList.remove('sticky-transition');
+  }, 150);
 };
 
-// Animate Title On load
-animatedTitle.style.setProperty('--animate-duration', '2.5s');
-animatedTitle.classList.add('animate__animated', 'animate__fadeInDown');
+const unStickHeader = () => navigation.classList.remove('sticky');
 
-// *** Scroll to Sections Events
+const animateFeatures = () =>
+  animatedFeatures.forEach((el, i) => {
+    const anim = el =>
+      el.classList.add('animate__animated', 'animate__fadeInUp');
+    setTimeout(() => anim(el), i * 150);
+  });
+
+const animateHowWorks = () =>
+  animatedMobile.classList.add('animate__animated', 'animate__fadeInLeft');
+
+const animateCities = () =>
+  animatedCities.forEach((el, i) => {
+    const anim = el => el.classList.add('animate__animated', 'animate__fadeIn');
+    setTimeout(() => anim(el), i * 200);
+  });
+
+const animatePlan = () =>
+  animatedPlan.classList.add('animate__animated', 'animate__pulse');
+
+const animateTitle = () => {
+  animatedTitle.style.setProperty('--animate-duration', '2.5s');
+  animatedTitle.classList.add('animate__animated', 'animate__fadeInDown');
+};
+// Animate Title On load
+animateTitle();
+
 const scrollToSection = (e, section) => {
   e.preventDefault();
   section.scrollIntoView({
@@ -79,10 +73,25 @@ const scrollToSection = (e, section) => {
   });
 };
 
-mainBtn.addEventListener('click', e => scrollToSection(e, sectionPlans));
+// *** Events
+window.onscroll = () => {
+  window.pageYOffset >= stickyPoint ? stickHeader() : unStickHeader();
+  window.pageYOffset >= sectionFeatures.offsetTop && animateFeatures();
+  window.pageYOffset >= sectionHowWorks.offsetTop - 50 && animateHowWorks();
+  window.pageYOffset >= sectionCities.offsetTop - 50 && animateCities();
+  window.pageYOffset >= sectionPlans.offsetTop - 50 && animatePlan();
+};
 
-secondaryBtn.addEventListener('click', e =>
+btnMain.addEventListener('click', e => scrollToSection(e, sectionPlans));
+
+btnSecondary.addEventListener('click', e =>
   scrollToSection(e, sectionFeatures)
+);
+
+btnBackToTop.addEventListener('click', e => scrollToSection(e, header));
+
+btnMobileNav.addEventListener('click', () =>
+  navUl.classList.toggle('transition-nav-in')
 );
 
 linkToFood.addEventListener('click', e => scrollToSection(e, sectionFeatures));
@@ -94,9 +103,3 @@ linkToHowWorks.addEventListener('click', e =>
 linkToCities.addEventListener('click', e => scrollToSection(e, sectionCities));
 
 linkToSignUp.addEventListener('click', e => scrollToSection(e, sectionPlans));
-
-backToTopBtn.addEventListener('click', e => scrollToSection(e, header));
-
-mobileNavBtn.addEventListener('click', () =>
-  navUl.classList.toggle('transition-nav-in')
-);
