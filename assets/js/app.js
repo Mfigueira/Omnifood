ff4870e9f37ace9eb9502c6cd21b6aad4897933f
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-05-28 17:13:51 -0300

    added Intersection Observer API for animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,14 @@
 // *** Elements/Selectors
 // * Sections
 const header = document.querySelector('header');
-const navigation = header.querySelector('nav');
-const navUl = navigation.querySelector('ul');
+const nav = header.querySelector('nav');
+const navUl = nav.querySelector('ul');
 const sectionFeatures = document.querySelector('.section-features');
 const sectionHowWorks = document.querySelector('.section-steps');
 const sectionCities = document.querySelector('.section-cities');
 const sectionPlans = document.querySelector('.section-plans');
 // * Animated elements
+const animatedSections = document.querySelectorAll('.animated-section');
 const animatedTitle = document.querySelector('h1');
 const animatedFeatures = document.querySelectorAll('.section-features .box');
 const animatedMobile = document.querySelector('.animated-mobile');
@@ -19,53 +20,77 @@ const animatedPlan = document.querySelector('.animated-plan');
 const btnMobileMenu = document.querySelector('.mobile-menu-btn');
 
 // *** Animations
-const stickHeader = () => {
-  navigation.classList.add('sticky-transition');
+const animations = {
+  features() {
+    animatedFeatures.forEach((el, i) => {
+      const anim = el =>
+        el.classList.add('animate__animated', 'animate__fadeInUp');
+      setTimeout(() => anim(el), i * 150);
+    });
+  },
+  steps() {
+    animatedMobile.classList.add('animate__animated', 'animate__fadeInLeft');
+  },
+  cities() {
+    animatedCities.forEach((el, i) => {
+      const anim = el =>
+        el.classList.add('animate__animated', 'animate__fadeIn');
+      setTimeout(() => anim(el), i * 200);
+    });
+  },
+  plans() {
+    animatedPlan.classList.add('animate__animated', 'animate__pulse');
+  },
+  title() {
+    animatedTitle.style.setProperty('--animate-duration', '2.5s');
+    animatedTitle.classList.add('animate__animated', 'animate__fadeInDown');
+  },
+};
+
+// * Animate Title On load
+animations.title();
+
+// *** Intersection Observer API for animations
+// * Sticky header
+const slideInHeader = (add = true) => {
+  nav.classList.add('sticky-transition');
   setTimeout(() => {
-    navigation.classList.add('sticky');
-    navigation.classList.remove('sticky-transition');
+    add ? nav.classList.add('sticky') : nav.classList.remove('sticky');
+    nav.classList.remove('sticky-transition');
   }, 150);
 };
 
-const unStickHeader = () => navigation.classList.remove('sticky');
-
-const animateFeatures = () =>
-  animatedFeatures.forEach((el, i) => {
-    const anim = el =>
-      el.classList.add('animate__animated', 'animate__fadeInUp');
-    setTimeout(() => anim(el), i * 150);
-  });
-
-const animateHowWorks = () =>
-  animatedMobile.classList.add('animate__animated', 'animate__fadeInLeft');
-
-const animateCities = () =>
-  animatedCities.forEach((el, i) => {
-    const anim = el => el.classList.add('animate__animated', 'animate__fadeIn');
-    setTimeout(() => anim(el), i * 200);
-  });
-
-const animatePlan = () =>
-  animatedPlan.classList.add('animate__animated', 'animate__pulse');
-
-const animateTitle = () => {
-  animatedTitle.style.setProperty('--animate-duration', '2.5s');
-  animatedTitle.classList.add('animate__animated', 'animate__fadeInDown');
+const stickHeader = entries => {
+  const [entry] = entries;
+  if (Math.abs(entry.boundingClientRect.y) < nav.getBoundingClientRect().height)
+    return;
+  !entry.isIntersecting ? slideInHeader() : slideInHeader(false);
 };
 
-// Animate Title On load
-animateTitle();
+const headerObserver = new IntersectionObserver(stickHeader, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${nav.getBoundingClientRect().height}px`,
+});
 
-// *** Events
-window.onscroll = () => {
-  window.pageYOffset >= sectionFeatures.offsetTop
-    ? stickHeader() || animateFeatures()
-    : unStickHeader();
-  window.pageYOffset >= sectionHowWorks.offsetTop - 50 && animateHowWorks();
-  window.pageYOffset >= sectionCities.offsetTop - 50 && animateCities();
-  window.pageYOffset >= sectionPlans.offsetTop - 50 && animatePlan();
+headerObserver.observe(header);
+
+// * Animated sections
+const animateSection = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  animations[entry.target.dataset.animate]();
+  observer.unobserve(entry.target);
 };
 
+const sectionObserver = new IntersectionObserver(animateSection, {
+  root: null,
+  threshold: 0.9,
+});
+
+animatedSections.forEach(section => sectionObserver.observe(section));
+
+// *** Events listeners
 btnMobileMenu.addEventListener('click', () =>
   navUl.classList.toggle('transition-nav-in')
 );
